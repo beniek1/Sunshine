a122cdd22ba9638aa3b2a6aa59eae8fe732face0
beniek_ <przemyslaw.naborczyk@gmail.com>
2016-10-07 15:13:30 +0200

    //Added zip code to preference activity.

--- app/src/main/java/com/udacity/przemek/sunshine/SettingsActivity.java
+++ app/src/main/java/com/udacity/przemek/sunshine/SettingsActivity.java
@@ -21,11 +21,10 @@ public class SettingsActivity extends PreferenceActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Add 'general' preferences, defined in the XML file
-        // TODO: Add preferences from XML
+        addPreferencesFromResource(R.xml.pref_general);
 
-        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
-        // updated when the preference changes.
-        // TODO: Add preferences
+        //Allows showing what value is already chosen for this preference.
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.zip_preference_key)));
     }
 
     /**
